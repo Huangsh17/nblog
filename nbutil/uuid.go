a0792715b79d5505407c772e99193fda406b2bbe
huangsonghui <huangsonghui@dm-ai.cn>
2020-12-11 09:43:45 +0800

    1.更新说明文档

--- nbutil/uuid.go
+++ nbutil/uuid.go
@@ -4,9 +4,9 @@ import (
 	"github.com/satori/go.uuid"
 )
 
-func Uuid() string{
+func Uuid() string {
 	// 创建UUID
 	var err error
-	var u1 = uuid.Must(uuid.NewV4(),err).String() //上文介绍的Version 4
+	var u1 = uuid.Must(uuid.NewV4(), err).String() //上文介绍的Version 4
 	return u1
 }
